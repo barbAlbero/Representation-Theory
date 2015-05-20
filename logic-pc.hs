e9c04191a9a6fd7b1eac2936a484eb8365c925da
Alberto Fiori <barbAlbero@users.noreply.github.com>
2015-05-20 18:19:03 +0200

    little improvement

--- logic-pc.hs
+++ logic-pc.hs
@@ -5,6 +5,11 @@ newPC a b:ls
 	| a == b = Just b
 	| otherwise = Nothing
 
+
+isWhiteSpace c = orPC' (map newTestPC [(\x -> '\n' /= x),(\x -> '\t' /= x),(\x -> ' ' /= x)])
+
+getFirstNonWhite f = 
+
 newTestPC ::  (a -> Bool) -> [a] -> Maybe a
 newTestPC f [] = Nothing
 newTestPC f x:_
@@ -12,6 +17,8 @@ newTestPC f x:_
 	| otherwise = Nothing
 
 newTestPC (\x -> a /= x) -- controlla se il primo carattere dello 'stream' è diverso da a
+
+nonePC = newTestPC (\_-> False)
 
 andPC f g [] = Nothing
 andPC f g b:[] = Nothing
@@ -39,6 +46,14 @@ orPC' f:fs x:xs = firstJust ((f x):(orPC' fs xs))
 notPC f Nothing = Just undefined
 notPC f Just _  = Nothing
 
+whilePC f [] = Nothing
+whilePC f x:xs 
+			|f x == Nothing = Nothing
+			|otherwise = Just (takeJust ( (f x) : whilePC f xs ) )
+				where takeJust [] = []
+				      takeJust Nothing:xs = takeJust xs
+					  takeJust (Just x):xs = x : takeJust xs 
+
 untilPC f [] = Nothing
 untilPC f b:bs 
 	| f b == Nothing = Nothing
@@ -46,7 +61,16 @@ untilPC f b:bs
 
 manyPC n f  = andPC' (replicate n f) 
 
-between f g h bs = (andPC' [f,g,h] bs) !! 1 
+
+between f g h = secondPC f (firstPC g h)
+
+secondPC f g bs = try (\z -> z !! 1) (andPC' [f,g] bs)
+				where try _ Nothing = Nothing
+				      try f Just x = Just (f x)
+firstPC f g bs = try (\z -> z !! 0) (andPC' [f,g] bs)
+				where try _ Nothing = Nothing
+				      try f Just x = Just (f x)
+
 
 checkMaybe = foldr1 f -- se nella lista ci sono solo dei Just porta fuori il Just altrimenti restituisce nothing
 	where f :: Maybe [a] -> Maybe a -> Maybe [a]
